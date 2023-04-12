0142ffb2adbaa0612070f11b542404c6d0428a9d
Youssef Abdullah <youssef.abdallah2015@hotmail.com>
2023-04-12 23:29:02 +0200

    attach tablayout to viewpager

--- app/src/main/java/com/example/taskaty/app/ui/fragments/home/PersonalTasksFragment.kt
+++ app/src/main/java/com/example/taskaty/app/ui/fragments/home/PersonalTasksFragment.kt
@@ -1,21 +1,16 @@
 package com.example.taskaty.app.ui.fragments.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import com.example.taskaty.databinding.FragmentPersonalTasksBinding
 import com.example.taskaty.app.ui.fragments.abstractFragments.BaseFragment
+import com.example.taskaty.databinding.FragmentPersonalTasksBinding
 
 
 class PersonalTasksFragment :
     BaseFragment<FragmentPersonalTasksBinding>(FragmentPersonalTasksBinding::inflate) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return binding.root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
 
