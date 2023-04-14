b58b2d3f3a9d1b99eb0554c9cf3e5828acb57535
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-04-14 14:49:12 +0200

    update login presenter

---
+++ app/src/main/java/com/example/taskaty/app/auth/login/LoginFragment.kt
@@ -0,0 +1,45 @@
+package com.example.taskaty.app.auth.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.taskaty.databinding.FragmentLoginBinding
+import com.example.taskaty.app.ui.fragments.abstractFragments.BaseFragment
+import com.example.taskaty.data.repositories.local.LocalAuthRepository
+import com.example.taskaty.data.repositories.remote.RemoteAuthRepository
+import com.example.taskaty.domain.interactors.AuthInteractor
+
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) , LoginContract.View{
+    private lateinit var presenter: LoginContract.Presenter
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        presenter = LoginPresenter(AuthInteractor(LocalAuthRepository.getInstance(requireContext()),RemoteAuthRepository.getInstance()),this)
+        setup()
+        return binding.root
+    }
+
+    private fun setup() {
+        presenter.login("ahmed","123456")
+    }
+
+    override fun showLoading() {
+        TODO("Not yet implemented")
+    }
+
+    override fun hideLoading() {
+        TODO("Not yet implemented")
+    }
+
+    override fun showErrorMessage(message: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun successLogin() {
+        TODO("Not yet implemented")
+    }
+
+}
