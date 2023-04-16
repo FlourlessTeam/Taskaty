31a3f6a4558d61cf1c44eac2a2ec3ad7df470706
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-04-16 09:43:36 +0200

    apply navigate to signup

--- app/src/main/java/com/example/taskaty/app/ui/fragments/OnBoardingFragment.kt
+++ app/src/main/java/com/example/taskaty/app/ui/fragments/OnBoardingFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.content.ContextCompat
 import com.example.taskaty.R
+import com.example.taskaty.app.auth.signup.SignupFragment
 import com.example.taskaty.databinding.FragmentOnboardingBinding
 import com.example.taskaty.app.ui.fragments.abstractFragments.BaseFragment
 
@@ -13,7 +14,23 @@ class OnBoardingFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         activity?.window?.statusBarColor = ContextCompat.getColor(requireContext(), R.color.primary_color)
+        setup()
+    }
 
+    private fun setup() {
+        with(binding) {
+            buttonStart.setOnClickListener {
+                navigateToSignupScreen()
+            }
+        }
+    }
+
+    private fun navigateToSignupScreen() {
+        val signupFragment = SignupFragment()
+        val transaction = requireActivity().supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container_fragment, signupFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 
 
