efa61e20bf2b01140adc68c75cd680fde4cb7dbb
Youssef Abdullah <youssef.abdallah2015@hotmail.com>
2023-04-16 19:31:43 +0200

    modify interactor

--- app/src/main/java/com/example/taskaty/domain/interactors/CardDataInteractor.kt
+++ app/src/main/java/com/example/taskaty/domain/interactors/CardDataInteractor.kt
@@ -1,36 +1,30 @@
 package com.example.taskaty.domain.interactors
 
+import com.example.taskaty.data.repositories.remote.RemoteTasksRepository
 import com.example.taskaty.data.response.RepoCallback
 import com.example.taskaty.data.response.RepoResponse
 import com.example.taskaty.domain.entities.Task
 import com.example.taskaty.domain.entities.TeamTask
-import com.example.taskaty.domain.repositories.remote.TasksDataSource
-import com.example.taskaty.domain.repositories.remote.TeamTasksDataSource
 
-class CardDataInteractor(
-    private val tasksDataSource: TasksDataSource,
-    private val teamTasksDataSource: TeamTasksDataSource,
-) {
+class CardDataInteractor(private val repo: RemoteTasksRepository) {
+
 
     fun getPersonalTasksData(statusType: Int, callback: RepoCallback<List<Task>>) {
-        tasksDataSource.getAllPersonalTasks(object : RepoCallback<List<Task>> {
+        repo.getAllPersonalTasks(object : RepoCallback<List<Task>> {
 
             override fun onSuccess(response: RepoResponse.Success<List<Task>>) {
-
                 val task = response.data.filter { it.status == statusType }
-
                 callback.onSuccess(RepoResponse.Success(task))
             }
 
             override fun onError(response: RepoResponse.Error<List<Task>>) {
-
                 callback.onError(RepoResponse.Error(response.message))
             }
         })
     }
 
     fun getTeamTasksData(statusType: Int, callback: RepoCallback<List<TeamTask>>) {
-        teamTasksDataSource.getAllTeamTasks(object : RepoCallback<List<TeamTask>> {
+        repo.getAllTeamTasks(object : RepoCallback<List<TeamTask>> {
 
             override fun onSuccess(response: RepoResponse.Success<List<TeamTask>>) {
 
@@ -38,6 +32,7 @@ class CardDataInteractor(
 
                 callback.onSuccess(RepoResponse.Success(teamTask))
             }
+
             override fun onError(response: RepoResponse.Error<List<TeamTask>>) {
 
                 callback.onError(RepoResponse.Error(response.message))
