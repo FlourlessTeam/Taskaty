aff9886ee404315c276cec04ae48eaa9946a6488
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-04-16 03:03:16 +0200

    update login fragment

--- app/src/main/java/com/example/taskaty/app/auth/login/LoginFragment.kt
+++ app/src/main/java/com/example/taskaty/app/auth/login/LoginFragment.kt
@@ -1,9 +1,8 @@
 package com.example.taskaty.app.auth.login
 
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import com.example.taskaty.databinding.FragmentLoginBinding
 import com.example.taskaty.app.ui.fragments.abstractFragments.BaseFragment
@@ -14,37 +13,43 @@ import com.example.taskaty.domain.interactors.AuthInteractor
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) , LoginContract.View{
     private lateinit var presenter: LoginContract.Presenter
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         presenter = LoginPresenter(AuthInteractor(LocalAuthRepository.getInstance(requireContext()),RemoteAuthRepository.getInstance()),this)
         setup()
-        return binding.root
     }
 
     private fun setup() {
         binding.buttonLogin.setOnClickListener {
             binding.apply {
-                presenter.login(editTextUsername.text.toString(),editTextPassword.text.toString())
+                presenter.onLogin(editTextUsername.text.toString(),editTextPassword.text.toString())
             }
         }
     }
 
     override fun showLoading() {
-        TODO("Not yet implemented")
+        Log.d("TAG", "showLoading: ")
     }
 
     override fun hideLoading() {
-        TODO("Not yet implemented")
+       Log.d("TAG", "hideLoading: ")
     }
 
     override fun showErrorMessage(message: String) {
-        Toast.makeText(requireContext(),message, Toast.LENGTH_LONG).show()
+        showToast(message)
     }
 
-    override fun successLogin() {
-        TODO("Not yet implemented")
+    override fun navigateToHomeScreen() {
+        showToast("Hi, ${binding.editTextUsername.text}")
+    }
+
+    override fun showValidationError(message: String) {
+        showToast(message)
+    }
+    private fun showToast(message: String) {
+        activity?.runOnUiThread {
+            Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
+        }
     }
 
 }
