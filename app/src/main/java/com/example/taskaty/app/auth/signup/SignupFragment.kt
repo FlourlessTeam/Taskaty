5abfe3b506bb87e9e16b6295ecbf45680db3f387
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-04-16 09:45:04 +0200

    apply mvp on signup fragment

--- app/src/main/java/com/example/taskaty/app/auth/signup/SignupFragment.kt
+++ app/src/main/java/com/example/taskaty/app/auth/signup/SignupFragment.kt
@@ -1,67 +1,91 @@
 package com.example.taskaty.app.auth.signup
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import com.example.taskaty.R
+import com.example.taskaty.app.auth.login.LoginFragment
 import com.example.taskaty.databinding.FragmentSignupBinding
 import com.example.taskaty.app.ui.fragments.abstractFragments.BaseFragment
 import com.example.taskaty.data.repositories.local.LocalAuthRepository
 import com.example.taskaty.data.repositories.remote.RemoteAuthRepository
-import com.example.taskaty.domain.entities.User
 import com.example.taskaty.domain.interactors.AuthInteractor
 
 
 class SignupFragment : BaseFragment<FragmentSignupBinding>(FragmentSignupBinding::inflate),
     SignupContract.View {
     private lateinit var presenter: SignupContract.Presenter
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.window?.statusBarColor = ContextCompat.getColor(requireContext(), R.color.primary_variant)
         presenter = SignupPresenter(
             AuthInteractor(
-                LocalAuthRepository.getInstance(requireContext()),
-                RemoteAuthRepository.getInstance()
+                LocalAuthRepository.getInstance(requireContext().applicationContext), RemoteAuthRepository.getInstance()
             ), this
         )
         setup()
-        return binding.root
     }
-
     private fun setup() {
-        binding.buttonSignup.setOnClickListener {
-            binding.apply {
-                if (editTextPassword.text.toString() != editTextConfirmPassword.text.toString()) {
-                    showErrorMessage("Password and Confirm Password must be the same")
-                } else {
-                    presenter.signup(
-                        User(
-                            editTextUsername.text.toString(),
-                            editTextPassword.text.toString()
-                        )
-                    )
-                }
+        with(binding) {
+            buttonSignup.setOnClickListener {
+                presenter.onSignup(
+                    editTextUsername.text.toString(),
+                    editTextPassword.text.toString(),
+                    editTextConfirmPassword.text.toString()
+                )
+            }
+            textGoLogin.setOnClickListener {
+                navigateToLoginScreen()
             }
         }
-
     }
 
     override fun showLoading() {
-        TODO("Not yet implemented")
+        activity?.runOnUiThread {
+            setVisibility(true)
+            binding.buttonSignup.text = ""
+        }
     }
 
     override fun hideLoading() {
-        TODO("Not yet implemented")
+        activity?.runOnUiThread {
+            setVisibility(false)
+            binding.buttonSignup.text = getString(R.string.text_signup)
+        }
     }
 
     override fun showErrorMessage(message: String) {
-        Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
+        showToast(message)
     }
 
-    override fun successSignup(user: User) {
-        Toast.makeText(requireContext(), "Hi, ${user.name}!", Toast.LENGTH_LONG).show()
+    override fun navigateToLoginScreen(user: String) {
+        showToast("Hi, ${user}!")
+        navigateToLoginScreen()
+    }
+
+    override fun showValidationError(message: String) {
+        showToast(message)
+    }
+    private fun navigateToLoginScreen() {
+        val loginFragment = LoginFragment()
+        val transaction = requireActivity().supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container_fragment, loginFragment)
+        transaction.addToBackStack("LoginFragment")
+        transaction.commit()
+    }
+
+    private fun showToast(message: String) {
+        activity?.runOnUiThread {
+            Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun setVisibility(isVisible: Boolean) {
+        with(binding) {
+            progressBar.isVisible = isVisible
+            buttonSignup.isEnabled = !isVisible
+        }
     }
 
 }
