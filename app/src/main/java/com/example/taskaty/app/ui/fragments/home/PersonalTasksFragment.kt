90600c3d6a3e9bf0c777d2edfb2a4d54ad9b96d2
Youssef Abdullah <youssef.abdallah2015@hotmail.com>
2023-04-16 19:33:21 +0200

    get tasks

--- app/src/main/java/com/example/taskaty/app/ui/fragments/home/PersonalTasksFragment.kt
+++ app/src/main/java/com/example/taskaty/app/ui/fragments/home/PersonalTasksFragment.kt
@@ -1,9 +1,15 @@
 package com.example.taskaty.app.ui.fragments.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.example.taskaty.app.ui.fragments.abstractFragments.BaseFragment
+import com.example.taskaty.data.repositories.remote.RemoteTasksRepository
+import com.example.taskaty.data.response.RepoCallback
+import com.example.taskaty.data.response.RepoResponse
 import com.example.taskaty.databinding.FragmentPersonalTasksBinding
+import com.example.taskaty.domain.entities.Task
+import com.example.taskaty.domain.interactors.CardDataInteractor
 
 
 class PersonalTasksFragment :
@@ -11,6 +17,29 @@ class PersonalTasksFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val interactor = CardDataInteractor(RemoteTasksRepository.getInstance("token"))
+        getPersonalTasksData(interactor, 0)
+        getPersonalTasksData(interactor, 1)
+        getPersonalTasksData(interactor, 2)
+    }
+
+
+    private fun getPersonalTasksData(interactor: CardDataInteractor, statusType: Int) {
+        interactor.getPersonalTasksData(statusType, object : RepoCallback<List<Task>> {
+            override fun onSuccess(response: RepoResponse.Success<List<Task>>) {
+                val tasks = response.data.filter { it.status == statusType }
+                //...
+
+            }
+
+            override fun onError(response: RepoResponse.Error<List<Task>>) {
+                Log.d("tag", "getPersonalTasksData onError: ${response.message}")
+
+            }
+
+        })
+
     }
 
 }
--- app/src/main/java/com/example/taskaty/app/ui/fragments/home/TeamTasksFragment.kt
+++ app/src/main/java/com/example/taskaty/app/ui/fragments/home/TeamTasksFragment.kt
@@ -1,15 +1,44 @@
 package com.example.taskaty.app.ui.fragments.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import com.example.taskaty.databinding.FragmentTeamTasksBinding
 import com.example.taskaty.app.ui.fragments.abstractFragments.BaseFragment
+import com.example.taskaty.data.repositories.remote.RemoteTasksRepository
+import com.example.taskaty.data.response.RepoCallback
+import com.example.taskaty.data.response.RepoResponse
+import com.example.taskaty.databinding.FragmentTeamTasksBinding
+import com.example.taskaty.domain.entities.TeamTask
+import com.example.taskaty.domain.interactors.CardDataInteractor
 
 class TeamTasksFragment :
     BaseFragment<FragmentTeamTasksBinding>(FragmentTeamTasksBinding::inflate) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        val interactor = CardDataInteractor(RemoteTasksRepository.getInstance("token"))
+        getTeamTasksData(interactor, 0)
+        getTeamTasksData(interactor, 1)
+        getTeamTasksData(interactor, 2)
+    }
+
+
+    private fun getTeamTasksData(interactor: CardDataInteractor, statusType: Int) {
+        interactor.getTeamTasksData(statusType, object : RepoCallback<List<TeamTask>> {
+            override fun onSuccess(response: RepoResponse.Success<List<TeamTask>>) {
+                val tasks = response.data.filter { it.task.status == statusType }
+                //...
+
+            }
+
+            override fun onError(response: RepoResponse.Error<List<TeamTask>>) {
+                Log.d("tag", "getTeamTasksData onError: ${response.message}")
+
+            }
+
+        })
 
     }
 }
