ddbc6f244d052922d4c5a0f86768483ad13229cb
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-04-16 02:59:22 +0200

    update auth interactor

--- app/src/main/java/com/example/taskaty/domain/interactors/AuthInteractor.kt
+++ app/src/main/java/com/example/taskaty/domain/interactors/AuthInteractor.kt
@@ -3,6 +3,8 @@ package com.example.taskaty.domain.interactors
 
 import com.example.taskaty.data.response.RepoCallback
 import com.example.taskaty.data.response.RepoResponse
+import com.example.taskaty.domain.entities.LoginResponse
+import com.example.taskaty.domain.entities.SignupResponse
 import com.example.taskaty.domain.entities.User
 import com.example.taskaty.domain.repositories.local.LocalAuthDataSource
 import com.example.taskaty.domain.repositories.remote.RemoteAuthDataSource
@@ -14,29 +16,29 @@ class AuthInteractor(
 ) {
 
     fun login(userName: String, password: String, callback: RepoCallback<String>) {
-        val token = getToken()
-        if (token.isNotEmpty()) {
-            callback.onSuccess(RepoResponse.Success(token))
-        } else {
             getTokenFromRemote(userName, password, callback)
-        }
     }
 
-    fun signup(user: User, callback: RepoCallback<User>) {
-        remoteAuthDataSource.signup(user, object : RepoCallback<User> {
-            override fun onSuccess(response: RepoResponse.Success<User>) {
-                callback.onSuccess(response)
+    fun signup(user: User, callback: RepoCallback<String>) {
+        remoteAuthDataSource.signup(user, object : RepoCallback<SignupResponse> {
+            override fun onSuccess(response: RepoResponse.Success<SignupResponse>) {
+                if (response.data.isSuccess){
+                    callback.onSuccess(RepoResponse.Success(response.data.value.username))
+                }
+                else{
+                    callback.onError(RepoResponse.Error(response.data.message.toString()))
+                }
             }
 
-            override fun onError(response: RepoResponse.Error<User>) {
-                callback.onError(response)
+            override fun onError(response: RepoResponse.Error<SignupResponse>) {
+                callback.onError(RepoResponse.Error(response.message))
             }
+
 
         })
     }
-
-    private fun getToken(): String {
-        return localAuthDataSource.getToken() ?: ""
+     fun getTokenFromLocal(): String {
+        return localAuthDataSource.getToken()
     }
 
     private fun getTokenFromRemote(
@@ -44,19 +46,27 @@ class AuthInteractor(
         password: String,
         callback: RepoCallback<String>,
     ) {
-        remoteAuthDataSource.fetchTokenByLogin(userName, password, object : RepoCallback<String> {
-            override fun onSuccess(response: RepoResponse.Success<String>) {
-                val token = response.data
-                localAuthDataSource.updateToken(token)
-                callback.onSuccess(RepoResponse.Success(token))
+        remoteAuthDataSource.fetchTokenByLogin(userName, password, object : RepoCallback<LoginResponse> {
+
+            override fun onSuccess(response: RepoResponse.Success<LoginResponse>) {
+                if (response.data.isSuccess) {
+                    val token = response.data.value.token
+                    callback.onSuccess(RepoResponse.Success(token))
+                    localAuthDataSource.updateToken(token)
+                }
+                else{
+                    callback.onError(RepoResponse.Error(response.data.message.toString()))
+                }
             }
 
-            override fun onError(response: RepoResponse.Error<String>) {
-                callback.onError(response)
+            override fun onError(response: RepoResponse.Error<LoginResponse>) {
+                callback.onError(RepoResponse.Error(response.message))
             }
         })
     }
-
+    fun checkValidField(userName: String, password: String,confirmPassword:String=password): Boolean {
+        return userName.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()
+    }
 }
 
 
