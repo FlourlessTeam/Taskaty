4000383fd18ea51f4dcd72b95ec8f4859e0f90d6
Abdelrahman Sedky <abdelrahmanoda@gmail.com>
2023-04-17 06:09:10 +0200

    add team task details logic

---
+++ app/src/main/java/com/example/taskaty/app/ui/fragments/details/team/TeamTaskDetailsPresenter.kt
@@ -0,0 +1,44 @@
+package com.example.taskaty.app.ui.fragments.details.team
+
+import com.example.taskaty.data.response.RepoCallback
+import com.example.taskaty.data.response.RepoResponse
+import com.example.taskaty.domain.entities.TeamTask
+import com.example.taskaty.domain.interactors.TeamTaskInteractor
+
+class TeamTaskDetailsPresenter(
+	private val view: IContract.IView,
+	private val teamTaskInteractor: TeamTaskInteractor
+) : IContract.IPresenter {
+
+	override fun getTeamTask(teamTaskId: String) {
+		view.showGetTeamTaskLoading()
+		teamTaskInteractor.getTeamTaskById(teamTaskId, object : RepoCallback<TeamTask> {
+			override fun onSuccess(response: RepoResponse.Success<TeamTask>) {
+				view.hideGetTeamTaskLoading()
+				view.updateUiData(response.data)
+			}
+
+			override fun onError(response: RepoResponse.Error<TeamTask>) {
+				view.showMessage(response.message)
+			}
+		})
+
+	}
+
+	override fun updateTeamTaskStatus(teamTaskId: String, status: Int) {
+		view.showUpdateTeamTaskLoading()
+		teamTaskInteractor.updateTeamTaskStatus(teamTaskId, status, object : RepoCallback<Unit> {
+			override fun onSuccess(response: RepoResponse.Success<Unit>) {
+				view.hideUpdateTeamTaskLoading()
+				view.showMessage("Team Task Updated Successfully")
+			}
+
+			override fun onError(response: RepoResponse.Error<Unit>) {
+				view.hideUpdateTeamTaskLoading()
+				view.showMessage("Update Team Task Failed")
+			}
+		})
+
+	}
+
+}
---
+++ app/src/main/java/com/example/taskaty/domain/interactors/TeamTaskInteractor.kt
@@ -0,0 +1,36 @@
+package com.example.taskaty.domain.interactors
+
+import com.example.taskaty.data.response.RepoCallback
+import com.example.taskaty.data.response.RepoResponse
+import com.example.taskaty.domain.entities.TeamTask
+import com.example.taskaty.domain.repositories.remote.TeamTasksDataSource
+
+class TeamTaskInteractor(
+	private val teamTasksDataSource: TeamTasksDataSource,
+) {
+	fun getTeamTaskById(teamTaskId: String, callback: RepoCallback<TeamTask>) {
+		teamTasksDataSource.getAllTeamTasks(object : RepoCallback<List<TeamTask>> {
+			override fun onSuccess(response: RepoResponse.Success<List<TeamTask>>) {
+				val teamTask = response.data.find { it.task.id == teamTaskId }!!
+				callback.onSuccess(RepoResponse.Success(teamTask))
+			}
+
+			override fun onError(response: RepoResponse.Error<List<TeamTask>>) {
+				val message = response.message
+				callback.onError(RepoResponse.Error(message))
+			}
+		})
+	}
+
+	fun updateTeamTaskStatus(teamTaskId: String, teamTaskStatus: Int, callback: RepoCallback<Unit>) {
+		teamTasksDataSource.updateTeamTaskState(teamTaskId, teamTaskStatus, object : RepoCallback<Unit> {
+			override fun onSuccess(response: RepoResponse.Success<Unit>) {
+				callback.onSuccess(RepoResponse.Success(Unit))
+			}
+
+			override fun onError(response: RepoResponse.Error<Unit>) {
+				callback.onSuccess(RepoResponse.Success(Unit))
+			}
+		})
+	}
+}
