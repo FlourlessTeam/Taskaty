3edab91f1343b844f947adc92cbac75c661e9406
أســـــاور <xyzas.7510@gmail.com>
2023-04-17 22:58:47 +0300

    done filter team task

--- app/src/main/java/com/example/taskaty/app/ui/fragments/home/TeamTasksFragment.kt
+++ app/src/main/java/com/example/taskaty/app/ui/fragments/home/TeamTasksFragment.kt
@@ -3,11 +3,14 @@ package com.example.taskaty.app.ui.fragments.home
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.core.view.isVisible
 import com.example.taskaty.app.ui.fragments.abstractFragments.BaseFragment
+import com.example.taskaty.app.ui.fragments.home.adapters.ParentPersonalAdapter
 import com.example.taskaty.data.repositories.remote.RemoteTasksRepository
 import com.example.taskaty.data.response.RepoCallback
 import com.example.taskaty.data.response.RepoResponse
 import com.example.taskaty.databinding.FragmentTeamTasksBinding
+import com.example.taskaty.domain.entities.Task
 import com.example.taskaty.domain.entities.TeamTask
 import com.example.taskaty.domain.interactors.CardDataInteractor
 
@@ -17,7 +20,9 @@ class TeamTasksFragment :
     private val token =
         "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJodHRwczovL3RoZS1jaGFuY2Uub3JnLyIsInN1YiI6ImMyMzY0MzdmLTZiMDktNGMyNC1iZDhmLTFmZmFhYmY5ZWVmZSIsInRlYW1JZCI6ImMyYzAyNTA3LTk5NjgtNDg2Yi05YmYwLTRjMzg2MGZlMWYyZCIsImlzcyI6Imh0dHBzOi8vdGhlLWNoYW5jZS5vcmcvIiwiZXhwIjoxNjgxODUzMjcyfQ.p02yBvXNP7npFkiegLO6aJTSrXjPtk91Urfwsuza-sQ"
     private val interactor = CardDataInteractor(RemoteTasksRepository.getInstance(token))
-
+    private var inProgressTasks = listOf<TeamTask>()
+    private var upcomingTasks = listOf<TeamTask>()
+        private var doneTasks = listOf<TeamTask>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -28,7 +33,7 @@ class TeamTasksFragment :
         interactor.getTeamTasksData(object : RepoCallback<List<TeamTask>> {
             override fun onSuccess(response: RepoResponse.Success<List<TeamTask>>) {
                 val tasks = response.data
-
+                filterTasks(tasks)
             }
 
             override fun onError(response: RepoResponse.Error<List<TeamTask>>) {
@@ -39,4 +44,27 @@ class TeamTasksFragment :
         })
 
     }
+
+    private fun filterTasks(tasks: List<TeamTask>) {
+        inProgressTasks = tasks.filter { it.task.status == PersonalTasksFragment.IN_PROGRESS_STATUS }
+        upcomingTasks = tasks.filter { it.task.status == PersonalTasksFragment.UPCOMING_STATUS }.take(
+            PersonalTasksFragment.LIMIT
+        )
+        doneTasks = tasks.filter { it.task.status == PersonalTasksFragment.DONE_STATUS }.take(
+            PersonalTasksFragment.LIMIT
+        )
+
+    }
+//    private fun initViews() {
+//        val adapter = ParentPersonalAdapter(inProgressTasks, upcomingTasks, doneTasks)
+//        binding.PersonalTasksRecycler.adapter = adapter
+//        showTasks()
+//    }
+
+    private fun showTasks() {
+        binding.apply {
+            PersonalTasksRecycler.isVisible = true
+        }
+    }
+
 }
