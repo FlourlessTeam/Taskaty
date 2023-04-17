82dc29854d9be8a83e74854c4fd3c3e6d3bc8a26
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-04-17 03:35:14 +0200

    change the status bar color of onBoarding fragment

--- app/src/main/java/com/example/taskaty/app/ui/fragments/OnBoardingFragment.kt
+++ app/src/main/java/com/example/taskaty/app/ui/fragments/OnBoardingFragment.kt
@@ -4,22 +4,16 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.content.ContextCompat
 import com.example.taskaty.R
-import com.example.taskaty.app.auth.login.LoginContract
 import com.example.taskaty.app.auth.login.LoginFragment
-import com.example.taskaty.app.auth.signup.SignupFragment
 import com.example.taskaty.databinding.FragmentOnboardingBinding
 import com.example.taskaty.app.ui.fragments.abstractFragments.BaseFragment
-import com.example.taskaty.data.repositories.local.LocalAuthRepository
-import com.example.taskaty.data.repositories.remote.RemoteAuthRepository
-import com.example.taskaty.domain.interactors.AuthInteractor
 
 
 class OnBoardingFragment :
     BaseFragment<FragmentOnboardingBinding>(FragmentOnboardingBinding::inflate) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        activity?.window?.statusBarColor =
-            ContextCompat.getColor(requireContext(), R.color.primary_color)
+        activity?.window?.statusBarColor = ContextCompat.getColor(requireContext(), R.color.primary_color)
         setup()
     }
 
