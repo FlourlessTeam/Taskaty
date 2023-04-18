dc654ffd893ba5c8db54f98001c735f44207c110
Zeyad Abdullah <zeyadAbdullah@gmail.com>
2023-04-18 06:03:14 +0200

    add mvp to personal tasks fragment

--- app/src/main/java/com/example/taskaty/app/ui/fragments/viewAll/personal/ViewAllPersonalTasksContract.kt
+++ app/src/main/java/com/example/taskaty/app/ui/fragments/viewAll/personal/ViewAllPersonalTasksContract.kt
@@ -1,4 +1,14 @@
 package com.example.taskaty.app.ui.fragments.viewAll.personal
 
+import com.example.taskaty.data.response.RepoCallback
+import com.example.taskaty.data.response.RepoResponse
+import com.example.taskaty.domain.entities.Task
+
 interface ViewAllPersonalTasksContract {
+    interface View{
+        fun viewAllPersonalTasksStatus(tasks: List<Task>)
+    }
+    interface Presenter{
+        fun getPersonalTasks()
+    }
 }
