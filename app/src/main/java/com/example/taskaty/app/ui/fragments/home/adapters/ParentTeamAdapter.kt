3a0d572c54795797b394824643938ca1227cca84
Zeyad Abdullah <zeyadAbdullah@gmail.com>
2023-04-20 18:11:50 +0200

    fix counting assignee

--- app/src/main/java/com/example/taskaty/app/ui/fragments/home/adapters/ParentTeamAdapter.kt
+++ app/src/main/java/com/example/taskaty/app/ui/fragments/home/adapters/ParentTeamAdapter.kt
@@ -21,233 +21,239 @@ import com.github.mikephil.charting.formatter.PercentFormatter
 import java.util.*
 
 class ParentTeamAdapter(
-	private val InProgress: List<TeamTask>,
-	private val Upcoming: List<TeamTask>,
-	private val Done: List<TeamTask>,
-	private val onViewAllClickListener: OnViewAllClickListener,
-	private val onTaskClickListener: OnTaskClickListener
+    private val InProgress: List<TeamTask>,
+    private val Upcoming: List<TeamTask>,
+    private val Done: List<TeamTask>,
+    private val onViewAllClickListener: OnViewAllClickListener,
+    private val onTaskClickListener: OnTaskClickListener
 ) : Adapter<ParentTeamAdapter.BaseViewHolder>() {
 
-	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
-		when (viewType) {
-			FIRST_ITEM -> {
-				val view = ChildRecyclerHomeChartBinding.inflate(
-					LayoutInflater.from(parent.context),
-					parent,
-					false
-				)
-				return ChartViewHolder(view.root)
-			}
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        when (viewType) {
+            FIRST_ITEM -> {
+                val view = ChildRecyclerHomeChartBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return ChartViewHolder(view.root)
+            }
 
-			SECOND_ITEM -> {
-				val view = ChildRecyclerHomeTeamInprogressBinding.inflate(
-					LayoutInflater.from(parent.context),
-					parent,
-					false
-				)
-				return InProgressViewHolder(view.root)
-			}
+            SECOND_ITEM -> {
+                val view = ChildRecyclerHomeTeamInprogressBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return InProgressViewHolder(view.root)
+            }
 
-			THIRD_ITEM -> {
-				val view = ChildRecyclerHomeTeamUpcomingBinding.inflate(
-					LayoutInflater.from(parent.context),
-					parent,
-					false
-				)
-				return UpcomingViewHolder(view.root)
-			}
+            THIRD_ITEM -> {
+                val view = ChildRecyclerHomeTeamUpcomingBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return UpcomingViewHolder(view.root)
+            }
 
-			else -> {
-				val view = ChildRecyclerHomeTeamDoneBinding.inflate(
-					LayoutInflater.from(parent.context),
-					parent,
-					false
-				)
-				return DoneViewHolder(view.root)
-			}
-		}
-	}
+            else -> {
+                val view = ChildRecyclerHomeTeamDoneBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return DoneViewHolder(view.root)
+            }
+        }
+    }
 
-	override fun getItemViewType(position: Int): Int {
-		return when (position) {
-			FIRST_ITEM -> FIRST_ITEM
-			SECOND_ITEM -> SECOND_ITEM
-			THIRD_ITEM -> THIRD_ITEM
-			else -> FOURTH_ITEM
-		}
-	}
+    override fun getItemViewType(position: Int): Int {
+        return when (position) {
+            FIRST_ITEM -> FIRST_ITEM
+            SECOND_ITEM -> SECOND_ITEM
+            THIRD_ITEM -> THIRD_ITEM
+            else -> FOURTH_ITEM
+        }
+    }
 
-	override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
-		when (holder) {
-			is ChartViewHolder -> bindChart(holder)
-			is InProgressViewHolder -> bindInProgress(holder)
-			is UpcomingViewHolder -> bindUpcoming(holder)
-			is DoneViewHolder -> bindDone(holder)
-		}
-	}
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        when (holder) {
+            is ChartViewHolder -> bindChart(holder)
+            is InProgressViewHolder -> bindInProgress(holder)
+            is UpcomingViewHolder -> bindUpcoming(holder)
+            is DoneViewHolder -> bindDone(holder)
+        }
+    }
 
-	private fun bindChart(holder: ChartViewHolder) {
-		val totalTasks = Done.size + InProgress.size + Upcoming.size
-		var upComingStatesValue = 0
-		var doneStatesValue = 0
-		var inProgressStatesValue = 0
-		if (totalTasks != 0) {
-			upComingStatesValue = (Upcoming.size * 100) / totalTasks
-			doneStatesValue = (Done.size * 100) / totalTasks
-			inProgressStatesValue = (InProgress.size * 100) / totalTasks
-		}
-		holder.binding.apply {
-			todoStates.text = upComingStatesValue.toString().plus("%")
-			doneStates.text = doneStatesValue.toString().plus("%")
-			inProgressStates.text = inProgressStatesValue.toString().plus("%")
-			chart.isDrawHoleEnabled = true
-			chart.setUsePercentValues(false)
-			chart.setDrawEntryLabels(false)
-			chart.holeRadius = 70f
-			chart.centerText = "Total \n$totalTasks"
-			chart.setCenterTextSize(11F)
-			chart.description.isEnabled = false
-			chart.legend.isEnabled = false
-			chartTitle.text = "Team Todo States"
-			val entries = ArrayList<PieEntry>()
-			entries.add(PieEntry(Upcoming.size * 1f, "Todo"))
-			entries.add(PieEntry(Done.size * 1f, "Done"))
-			entries.add(PieEntry(InProgress.size * 1f, "In Progress"))
-			val colors = ArrayList<Int>()
-			colors.add(Color.parseColor("#7FBAA9"))
-			colors.add(Color.parseColor("#93CB80"))
-			colors.add(Color.parseColor("#418E77"))
-			val dataSet = PieDataSet(entries, "")
-			dataSet.colors = colors
-			val data = PieData(dataSet)
-			data.setDrawValues(false)
-			data.setValueFormatter(PercentFormatter(chart))
-			data.setValueTextSize(12f)
-			data.setValueTextColor(Color.BLACK)
-			chart.data = data
-			chart.invalidate()
-		}
-	}
+    private fun bindChart(holder: ChartViewHolder) {
+        val totalTasks = Done.size + InProgress.size + Upcoming.size
+        var upComingStatesValue = 0
+        var doneStatesValue = 0
+        var inProgressStatesValue = 0
+        if (totalTasks != 0) {
+            upComingStatesValue = (Upcoming.size * 100) / totalTasks
+            doneStatesValue = (Done.size * 100) / totalTasks
+            inProgressStatesValue = (InProgress.size * 100) / totalTasks
+        }
+        holder.binding.apply {
+            todoStates.text = upComingStatesValue.toString().plus("%")
+            doneStates.text = doneStatesValue.toString().plus("%")
+            inProgressStates.text = inProgressStatesValue.toString().plus("%")
+            chart.isDrawHoleEnabled = true
+            chart.setUsePercentValues(false)
+            chart.setDrawEntryLabels(false)
+            chart.holeRadius = 70f
+            chart.centerText = "Total \n$totalTasks"
+            chart.setCenterTextSize(11F)
+            chart.description.isEnabled = false
+            chart.legend.isEnabled = false
+            chartTitle.text = "Team Todo States"
+            val entries = ArrayList<PieEntry>()
+            entries.add(PieEntry(Upcoming.size * 1f, "Todo"))
+            entries.add(PieEntry(Done.size * 1f, "Done"))
+            entries.add(PieEntry(InProgress.size * 1f, "In Progress"))
+            val colors = ArrayList<Int>()
+            colors.add(Color.parseColor("#7FBAA9"))
+            colors.add(Color.parseColor("#93CB80"))
+            colors.add(Color.parseColor("#418E77"))
+            val dataSet = PieDataSet(entries, "")
+            dataSet.colors = colors
+            val data = PieData(dataSet)
+            data.setDrawValues(false)
+            data.setValueFormatter(PercentFormatter(chart))
+            data.setValueTextSize(12f)
+            data.setValueTextColor(Color.BLACK)
+            chart.data = data
+            chart.invalidate()
+        }
+    }
 
 
-	private fun bindInProgress(holder: InProgressViewHolder) {
-		holder.binding.apply {
-			tasksNumber.text = InProgress.size.toString()
-			if (InProgress.isNotEmpty()) {
-				val adapter = ChildTeamInProgressAdapter(InProgress, onTaskClickListener)
-				inProgressViewAll.setOnClickListener { onViewAllClickListener.onViewAllClick(1) }
-				childRecycler.adapter = adapter
-			}
-		}
-	}
+    private fun bindInProgress(holder: InProgressViewHolder) {
+        holder.binding.apply {
+            tasksNumber.text = InProgress.size.toString()
+            if (InProgress.isNotEmpty()) {
+                val adapter = ChildTeamInProgressAdapter(InProgress, onTaskClickListener)
+                inProgressViewAll.setOnClickListener { onViewAllClickListener.onViewAllClick(1) }
+                childRecycler.adapter = adapter
+            }
+        }
+    }
 
-	private fun bindUpcoming(holder: UpcomingViewHolder) {
-		holder.binding.apply {
-			tasksNumber.text = Upcoming.size.toString()
-			if (Upcoming.isEmpty()) {
-				disappearCards(upcomingFirstCard, upcomingSecondCard, false)
-			} else if (Upcoming.size == 1) {
-				upcomingViewAll.setOnClickListener { onViewAllClickListener.onViewAllClick(0) }
-				val firstItem = Upcoming[FIRST_ITEM]
-				taskHeaderFirst.text = firstItem.title
-				dateTextFirst.text = DateTimeUtils.toDateFormat(firstItem.creationTime)
-				timeTextFirst.text = DateTimeUtils.toTimeFormat(firstItem.creationTime)
-				topAssigneeCountCircle.text = firstItem.assignee.split(",").size.toString()
-				upcomingFirstCard.setOnClickListener { onTaskClickListener.onTaskClick(firstItem) }
-				disappearCards(upcomingFirstCard, upcomingSecondCard, true)
-			} else {
-				upcomingViewAll.setOnClickListener { onViewAllClickListener.onViewAllClick(0) }
-				val firstItem = Upcoming[FIRST_ITEM]
-				val secondItem = Upcoming[SECOND_ITEM]
-				taskHeaderFirst.text = firstItem.title
-				dateTextFirst.text = DateTimeUtils.toDateFormat(firstItem.creationTime)
-				timeTextFirst.text = DateTimeUtils.toTimeFormat(firstItem.creationTime)
-				topAssigneeCountCircle.text = firstItem.assignee.split(",").size.toString()
-				taskHeaderSecond.text = secondItem.title
-				dateTextSecond.text = DateTimeUtils.toDateFormat(secondItem.creationTime)
-				timeTextSecond.text = DateTimeUtils.toTimeFormat(secondItem.creationTime)
-				bottomAssigneeCountCircle.text = secondItem.assignee.split(",").size.toString()
-				upcomingFirstCard.setOnClickListener { onTaskClickListener.onTaskClick(firstItem) }
-				upcomingSecondCard.setOnClickListener { onTaskClickListener.onTaskClick(secondItem) }
-			}
-		}
-	}
+    private fun bindUpcoming(holder: UpcomingViewHolder) {
+        holder.binding.apply {
+            tasksNumber.text = Upcoming.size.toString()
+            if (Upcoming.isEmpty()) {
+                disappearCards(upcomingFirstCard, upcomingSecondCard, false)
+            } else if (Upcoming.size == 1) {
+                upcomingViewAll.setOnClickListener { onViewAllClickListener.onViewAllClick(0) }
+                val firstItem = Upcoming[FIRST_ITEM]
+                taskHeaderFirst.text = firstItem.title
+                dateTextFirst.text = DateTimeUtils.toDateFormat(firstItem.creationTime)
+                timeTextFirst.text = DateTimeUtils.toTimeFormat(firstItem.creationTime)
+                topAssigneeCountCircle.text =
+                    firstItem.assignee.split(",").filter { it != "" }.size.toString()
+                upcomingFirstCard.setOnClickListener { onTaskClickListener.onTaskClick(firstItem) }
+                disappearCards(upcomingFirstCard, upcomingSecondCard, true)
+            } else {
+                upcomingViewAll.setOnClickListener { onViewAllClickListener.onViewAllClick(0) }
+                val firstItem = Upcoming[FIRST_ITEM]
+                val secondItem = Upcoming[SECOND_ITEM]
+                taskHeaderFirst.text = firstItem.title
+                dateTextFirst.text = DateTimeUtils.toDateFormat(firstItem.creationTime)
+                timeTextFirst.text = DateTimeUtils.toTimeFormat(firstItem.creationTime)
+                topAssigneeCountCircle.text =
+                    firstItem.assignee.split(",").filter { it != "" }.size.toString()
+                taskHeaderSecond.text = secondItem.title
+                dateTextSecond.text = DateTimeUtils.toDateFormat(secondItem.creationTime)
+                timeTextSecond.text = DateTimeUtils.toTimeFormat(secondItem.creationTime)
+                bottomAssigneeCountCircle.text =
+                    secondItem.assignee.split(",").filter { it != "" }.size.toString()
+                upcomingFirstCard.setOnClickListener { onTaskClickListener.onTaskClick(firstItem) }
+                upcomingSecondCard.setOnClickListener { onTaskClickListener.onTaskClick(secondItem) }
+            }
+        }
+    }
 
-	private fun bindDone(holder: DoneViewHolder) {
-		holder.binding.apply {
-			tasksNumber.text = Done.size.toString()
-			if (Done.isEmpty()) {
-				disappearCards(firstCard, secondCard, false)
-			} else if (Done.size == 1) {
-				doneViewAll.setOnClickListener { onViewAllClickListener.onViewAllClick(2) }
-				val firstItem = Done[FIRST_ITEM]
-				taskHeaderFirst.text = firstItem.title
-				dateTextFirst.text = DateTimeUtils.toDateFormat(firstItem.creationTime)
-				timeTextFirst.text = DateTimeUtils.toTimeFormat(firstItem.creationTime)
-				topAssigneeCountCircle.text = firstItem.assignee.split(",").size.toString()
-				firstCard.setOnClickListener { onTaskClickListener.onTaskClick(firstItem) }
-				disappearCards(firstCard, secondCard, true)
-			} else {
-				doneViewAll.setOnClickListener { onViewAllClickListener.onViewAllClick(2) }
-				val firstItem = Done[FIRST_ITEM]
-				val secondItem = Done[SECOND_ITEM]
-				taskHeaderFirst.text = firstItem.title
-				dateTextFirst.text = DateTimeUtils.toDateFormat(firstItem.creationTime)
-				timeTextFirst.text = DateTimeUtils.toTimeFormat(firstItem.creationTime)
-				topAssigneeCountCircle.text = firstItem.assignee.split(",").size.toString()
-				taskHeaderSecond.text = secondItem.title
-				dateTextSecond.text = DateTimeUtils.toDateFormat(secondItem.creationTime)
-				timeTextSecond.text = DateTimeUtils.toTimeFormat(secondItem.creationTime)
-				bottomAssigneeCountCircle.text = secondItem.assignee.split(",").size.toString()
-				firstCard.setOnClickListener { onTaskClickListener.onTaskClick(firstItem) }
-				secondCard.setOnClickListener { onTaskClickListener.onTaskClick(secondItem) }
-			}
-		}
-	}
+    private fun bindDone(holder: DoneViewHolder) {
+        holder.binding.apply {
+            tasksNumber.text = Done.size.toString()
+            if (Done.isEmpty()) {
+                disappearCards(firstCard, secondCard, false)
+            } else if (Done.size == 1) {
+                doneViewAll.setOnClickListener { onViewAllClickListener.onViewAllClick(2) }
+                val firstItem = Done[FIRST_ITEM]
+                taskHeaderFirst.text = firstItem.title
+                dateTextFirst.text = DateTimeUtils.toDateFormat(firstItem.creationTime)
+                timeTextFirst.text = DateTimeUtils.toTimeFormat(firstItem.creationTime)
+                topAssigneeCountCircle.text =
+                    firstItem.assignee.split(",").filter { it != "" }.size.toString()
+                firstCard.setOnClickListener { onTaskClickListener.onTaskClick(firstItem) }
+                disappearCards(firstCard, secondCard, true)
+            } else {
+                doneViewAll.setOnClickListener { onViewAllClickListener.onViewAllClick(2) }
+                val firstItem = Done[FIRST_ITEM]
+                val secondItem = Done[SECOND_ITEM]
+                taskHeaderFirst.text = firstItem.title
+                dateTextFirst.text = DateTimeUtils.toDateFormat(firstItem.creationTime)
+                timeTextFirst.text = DateTimeUtils.toTimeFormat(firstItem.creationTime)
+                topAssigneeCountCircle.text =
+                    firstItem.assignee.split(",").filter { it != "" }.size.toString()
+                taskHeaderSecond.text = secondItem.title
+                dateTextSecond.text = DateTimeUtils.toDateFormat(secondItem.creationTime)
+                timeTextSecond.text = DateTimeUtils.toTimeFormat(secondItem.creationTime)
+                bottomAssigneeCountCircle.text =
+                    secondItem.assignee.split(",").filter { it != "" }.size.toString()
+                firstCard.setOnClickListener { onTaskClickListener.onTaskClick(firstItem) }
+                secondCard.setOnClickListener { onTaskClickListener.onTaskClick(secondItem) }
+            }
+        }
+    }
 
-	private fun disappearCards(firsCard: CardView, secondCard: CardView, isOne: Boolean) {
-		if (isOne) {
-			secondCard.isVisible = false
-			val layoutParam = secondCard.layoutParams
-			layoutParam.height = 0
-			secondCard.layoutParams = layoutParam
-		} else {
-			firsCard.isVisible = false
-			secondCard.isVisible = false
-			val firstCardParam = firsCard.layoutParams
-			firstCardParam.height = 0
-			firsCard.layoutParams = firstCardParam
-			val secondCardParam = secondCard.layoutParams
-			secondCardParam.height = 0
-			secondCard.layoutParams = secondCardParam
-		}
-	}
+    private fun disappearCards(firsCard: CardView, secondCard: CardView, isOne: Boolean) {
+        if (isOne) {
+            secondCard.isVisible = false
+            val layoutParam = secondCard.layoutParams
+            layoutParam.height = 0
+            secondCard.layoutParams = layoutParam
+        } else {
+            firsCard.isVisible = false
+            secondCard.isVisible = false
+            val firstCardParam = firsCard.layoutParams
+            firstCardParam.height = 0
+            firsCard.layoutParams = firstCardParam
+            val secondCardParam = secondCard.layoutParams
+            secondCardParam.height = 0
+            secondCard.layoutParams = secondCardParam
+        }
+    }
 
-	override fun getItemCount() = FOURTH_ITEM + 1
+    override fun getItemCount() = FOURTH_ITEM + 1
 
-	abstract class BaseViewHolder(view: View) : ViewHolder(view)
+    abstract class BaseViewHolder(view: View) : ViewHolder(view)
 
-	class InProgressViewHolder(view: View) : BaseViewHolder(view) {
-		val binding = ChildRecyclerHomeTeamInprogressBinding.bind(view)
-	}
+    class InProgressViewHolder(view: View) : BaseViewHolder(view) {
+        val binding = ChildRecyclerHomeTeamInprogressBinding.bind(view)
+    }
 
-	class UpcomingViewHolder(view: View) : BaseViewHolder(view) {
-		val binding = ChildRecyclerHomeTeamUpcomingBinding.bind(view)
-	}
+    class UpcomingViewHolder(view: View) : BaseViewHolder(view) {
+        val binding = ChildRecyclerHomeTeamUpcomingBinding.bind(view)
+    }
 
-	class ChartViewHolder(view: View) : BaseViewHolder(view) {
-		val binding = ChildRecyclerHomeChartBinding.bind(view)
-	}
+    class ChartViewHolder(view: View) : BaseViewHolder(view) {
+        val binding = ChildRecyclerHomeChartBinding.bind(view)
+    }
 
-	class DoneViewHolder(view: View) : BaseViewHolder(view) {
-		val binding = ChildRecyclerHomeTeamDoneBinding.bind(view)
-	}
+    class DoneViewHolder(view: View) : BaseViewHolder(view) {
+        val binding = ChildRecyclerHomeTeamDoneBinding.bind(view)
+    }
 
-	companion object {
-		const val FIRST_ITEM = 0
-		const val SECOND_ITEM = 1
-		const val THIRD_ITEM = 2
-		const val FOURTH_ITEM = 3
-	}
+    companion object {
+        const val FIRST_ITEM = 0
+        const val SECOND_ITEM = 1
+        const val THIRD_ITEM = 2
+        const val FOURTH_ITEM = 3
+    }
 
 }
--- app/src/main/java/com/example/taskaty/app/ui/fragments/viewAll/team/ViewAllTeamTasksAdapter.kt
+++ app/src/main/java/com/example/taskaty/app/ui/fragments/viewAll/team/ViewAllTeamTasksAdapter.kt
@@ -16,67 +16,67 @@ import com.example.taskaty.domain.entities.TeamTask
 import com.example.taskaty.app.utils.DateTimeUtils
 
 class ViewAllTeamTasksAdapter(private val onTaskClickListener: OnTaskClickListener) :
-	ListAdapter<TeamTask, ViewAllTeamTasksAdapter.ViewAllHolder>(TeamTaskDiffCallback()) {
+    ListAdapter<TeamTask, ViewAllTeamTasksAdapter.ViewAllHolder>(TeamTaskDiffCallback()) {
 
 
-	class ViewAllHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-		val binding = ItemTeamViewAllBinding.bind(itemView)
-	}
+    class ViewAllHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val binding = ItemTeamViewAllBinding.bind(itemView)
+    }
 
-	class TeamTaskDiffCallback : DiffUtil.ItemCallback<TeamTask>() {
-		override fun areItemsTheSame(oldItem: TeamTask, newItem: TeamTask): Boolean {
-			return oldItem.id == newItem.id
-		}
+    class TeamTaskDiffCallback : DiffUtil.ItemCallback<TeamTask>() {
+        override fun areItemsTheSame(oldItem: TeamTask, newItem: TeamTask): Boolean {
+            return oldItem.id == newItem.id
+        }
 
-		override fun areContentsTheSame(oldItem: TeamTask, newItem: TeamTask): Boolean {
-			return oldItem == newItem
-		}
-	}
+        override fun areContentsTheSame(oldItem: TeamTask, newItem: TeamTask): Boolean {
+            return oldItem == newItem
+        }
+    }
 
-	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewAllHolder {
-		val view =
-			LayoutInflater.from(parent.context).inflate(R.layout.item_team_view_all, parent, false)
-		return ViewAllHolder(view)
-	}
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewAllHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_team_view_all, parent, false)
+        return ViewAllHolder(view)
+    }
 
 
-	override fun onBindViewHolder(holder: ViewAllHolder, position: Int) {
-		val item = getItem(position)
-		holder.binding.apply {
-			root.setOnClickListener { onTaskClickListener.onTaskClick(item) }
-			textTitle.text = item.title
-			textContent.text = item.description
-			textState.text = getStatusNames(item.status)
-			textState.backgroundTintList = ContextCompat.getColorStateList(
-				holder.itemView.context, getStatusColors(item.status)
-			)
-			textAssignee.text = item.assignee.split(",").size.toString()
+    override fun onBindViewHolder(holder: ViewAllHolder, position: Int) {
+        val item = getItem(position)
+        holder.binding.apply {
+            root.setOnClickListener { onTaskClickListener.onTaskClick(item) }
+            textTitle.text = item.title
+            textContent.text = item.description
+            textState.text = getStatusNames(item.status)
+            textState.backgroundTintList = ContextCompat.getColorStateList(
+                holder.itemView.context, getStatusColors(item.status)
+            )
+            textAssignee.text = item.assignee.split(",").filter { it != "" }.size.toString()
 
-			textAssignee.backgroundTintList = ContextCompat.getColorStateList(
-				holder.itemView.context, getStatusColors(item.status)
-			)
-			val inputDateString = item.creationTime
-			textCalender.text = DateTimeUtils.toDateFormat(inputDateString)
-			textClock.text = DateTimeUtils.toTimeFormat(inputDateString)
-		}
+            textAssignee.backgroundTintList = ContextCompat.getColorStateList(
+                holder.itemView.context, getStatusColors(item.status)
+            )
+            val inputDateString = item.creationTime
+            textCalender.text = DateTimeUtils.toDateFormat(inputDateString)
+            textClock.text = DateTimeUtils.toTimeFormat(inputDateString)
+        }
 
-	}
+    }
 
-	private fun getStatusNames(status: Int?): String {
-		return when (status) {
-			0 -> "ToDo"
-			1 -> "In Progress"
-			else -> "Done"
-		}
-	}
+    private fun getStatusNames(status: Int?): String {
+        return when (status) {
+            0 -> "ToDo"
+            1 -> "In Progress"
+            else -> "Done"
+        }
+    }
 
-	private fun getStatusColors(status: Int?): Int {
-		return when (status) {
-			0 -> R.color.todo_color
-			1 -> R.color.in_progress_color
-			else -> R.color.done_color
-		}
-	}
+    private fun getStatusColors(status: Int?): Int {
+        return when (status) {
+            0 -> R.color.todo_color
+            1 -> R.color.in_progress_color
+            else -> R.color.done_color
+        }
+    }
 
 
 }
