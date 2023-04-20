074921d231bc82c32228aa8af2b053dbfc56a656
Abdelrahman Mohamed <Abdelrahmanmohamed82@outlook.com>
2023-04-20 02:17:14 +0200

    changes

--- app/src/main/java/com/example/taskaty/app/ui/fragments/home/bottomSheet/NewTaskBottomSheetFragment.kt
+++ app/src/main/java/com/example/taskaty/app/ui/fragments/home/bottomSheet/NewTaskBottomSheetFragment.kt
@@ -17,8 +17,7 @@ class NewTaskBottomSheetFragment(private val selectedTabPosition: Int) : BottomS
     RepoCallback<Unit> {
 
     private lateinit var binding: FragmentAddTaskBottomSheetBinding
-    private val presenter: NewTaskPresenter = NewTaskPresenter()
-
+    private val presenter = NewTaskPresenter()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
@@ -32,8 +31,6 @@ class NewTaskBottomSheetFragment(private val selectedTabPosition: Int) : BottomS
         setupAddTaskBottomSheet()
     }
     private fun setupAddTaskBottomSheet() {
-        //selectedTabPosition when equal 0 means we in personal task fragment
-        //selectedTabPosition when equal 1 means we in team task fragment
 
         binding.newTaskBtn.setOnClickListener {
             presenter.onCreateBtnClicked(
@@ -54,6 +51,7 @@ class NewTaskBottomSheetFragment(private val selectedTabPosition: Int) : BottomS
     }
     override fun onSuccess(response: RepoResponse.Success<Unit>) {
       requireActivity().runOnUiThread {
+
           Toast.makeText(requireContext(), "Task Created Successfully", Toast.LENGTH_SHORT).show()
           dismiss()
       }
