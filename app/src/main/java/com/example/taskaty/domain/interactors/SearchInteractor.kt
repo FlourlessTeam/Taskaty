93355459048cd5778a3dbb0a1683c0e167bebb6a
Abdelwahab Elsayed <bdmstf@gmail.com>
2023-04-20 04:27:53 +0200

    hot fix /search interactor

--- app/src/main/java/com/example/taskaty/domain/interactors/SearchInteractor.kt
+++ app/src/main/java/com/example/taskaty/domain/interactors/SearchInteractor.kt
@@ -4,27 +4,68 @@ import com.example.taskaty.data.repositories.AllTasksRepositoryImpl
 import com.example.taskaty.data.response.RepoCallback
 import com.example.taskaty.data.response.RepoResponse
 import com.example.taskaty.domain.entities.PersonalTask
+import com.example.taskaty.domain.entities.Task
 import com.example.taskaty.domain.entities.TeamTask
 
 class SearchInteractor(private val repo: AllTasksRepositoryImpl) {
 
-    fun searchTasks (title: String, callback: RepoCallback<List<PersonalTask>>) {
+    fun searchTasks(title: String, callback: RepoCallback<List<Task>>) {
+
+        var isPersonalTaskLoaded = false
+        var isTeamTasksLoaded = false
+        var personalTasks: List<PersonalTask> = listOf()
+        var teamTasks: List<TeamTask> = listOf()
         repo.getAllPersonalTasks(object : RepoCallback<List<PersonalTask>> {
             override fun onSuccess(response: RepoResponse.Success<List<PersonalTask>>) {
-                val taskSearch = response.data.filter { it.title.contains(title)  }
-                callback.onSuccess(RepoResponse.Success(taskSearch))
+                isPersonalTaskLoaded = true
+                personalTasks = response.data
+                if (isTeamTasksLoaded)
+                    callback.onSuccess(
+                        RepoResponse.Success(
+                            filterTasks(
+                                title,
+                                personalTasks + teamTasks
+                            )
+                        )
+                    )
             }
 
             override fun onError(response: RepoResponse.Error<List<PersonalTask>>) {
                 callback.onError(RepoResponse.Error(response.message))
             }
         })
+        repo.getAllTeamTasks(object : RepoCallback<List<TeamTask>> {
+            override fun onSuccess(response: RepoResponse.Success<List<TeamTask>>) {
+                isTeamTasksLoaded = true
+                teamTasks = response.data
+                if (isPersonalTaskLoaded)
+                    callback.onSuccess(
+                        RepoResponse.Success(
+                            filterTasks(
+                                title,
+                                personalTasks + teamTasks
+                            )
+                        )
+                    )
+            }
+
+            override fun onError(response: RepoResponse.Error<List<TeamTask>>) {
+                callback.onError(RepoResponse.Error(response.message))
+            }
+
+        })
+    }
+
+    private fun filterTasks(text: String, tasks: List<Task>): List<Task> {
+
+        return tasks.filter { it.title.contains(text) }
     }
 
     fun searchTeamTasks(status: Int, title: String, callback: RepoCallback<List<TeamTask>>) {
         repo.getAllTeamTasks(object : RepoCallback<List<TeamTask>> {
             override fun onSuccess(response: RepoResponse.Success<List<TeamTask>>) {
-                val teamSearchTasks = response.data.filter { it.title.contains(title) && it.status == status }
+                val teamSearchTasks =
+                    response.data.filter { it.title.contains(title) && it.status == status }
                 callback.onSuccess(RepoResponse.Success(teamSearchTasks))
             }
 
